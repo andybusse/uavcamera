25f7ebf9eb1033822f7f671c7849f0cd08b1daa7
Piyabhum Sornpaisarn <ps26g08@ecs.soton.ac.uk>
2011-10-18 10:10:24 +0000

    this is uav connector
    git-svn-id: svn+ssh://forge.ecs.soton.ac.uk/projects/uavcamera@30 ea005762-10ae-0410-ac34-b80da5e0b649

---
+++ uav_connector/portConnector/Connector.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Configuration;
+using System.Net.Sockets;
+using System.Collections.Specialized;
+using System.Collections;
+using System.Threading;
+
+namespace portConnector
+{
+    class Connector
+    {
+        static int requestCounter;
+        static ArrayList hostData = new ArrayList();
+        static StringCollection hostNames = new StringCollection();
+
+        Socket UAV = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public byte[] dataFromUAV()
+        {
+            byte[] uavData = new byte[24];
+            byte[] testMessage = { 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0 };
+            try
+            {
+                int sendByte = UAV.Send(testMessage, testMessage.Length, SocketFlags.None);
+                Console.WriteLine("Sent {0} bytes.", sendByte);
+                int size = 24;
+
+                while (true)
+                {
+                    int byteCount = UAV.Receive(uavData, size, SocketFlags.None);
+                   //Console.WriteLine( Encoding.Unicode.GetChars(uavData));
+                   Console.WriteLine(Encoding.UTF8.GetString(uavData));
+                }
+            }
+            catch(SocketException uavMessage)
+            {
+                Console.WriteLine("{0} Error code:{1}.", uavMessage.Message, uavMessage.ErrorCode);
+            }
+            
+            return uavData;
+        }
+
+        public void ConnectToPort()
+        {
+            const Int32 portNumber = 8802;
+            const string portName = "localhost";
+            try
+            {
+                UAV.Connect("localhost", portNumber);
+                Console.WriteLine("Connection to Port Name:{0}; Port Number{1} is complete!",portName,portNumber);
+            }
+            catch (SocketException uavMessage)
+            {
+                Console.WriteLine("{0} Error code:{1}.", uavMessage.Message, uavMessage.ErrorCode);
+            }
+            
+        }
+
+
+    }
+}
---
+++ uav_connector/portConnector/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Configuration;
+using System.Net.Sockets;
+using System.Collections.Specialized;
+using System.Collections;
+using System.Threading;
+
+namespace portConnector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[256];
+            Connector connector1 = new Connector();
+            connector1.ConnectToPort();
+            data = connector1.dataFromUAV();
+        }
+    }
+}
